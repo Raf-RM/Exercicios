83b7a812632416a34c7937c50cbbb63a70ebf1bb
Raf-RM <rafael.r.mateus@gmail.com>
2022-02-16 23:48:52 -0300

    Exercícios estudo - 16/02/22

--- Exercicios-Simples-Python/portas-logicas.py
+++ Exercicios-Simples-Python/portas-logicas.py
@@ -88,6 +88,26 @@ class NotGate(UnaryGate):
 
         return 1 if entrada == 0 else 0
 
+class NandGate(BinaryGate):
+    def __init__(self,n):
+        BinaryGate.__init__(self,n)
+
+    def performGateLogic(self):
+        a = self.getEntradaA()
+        b = self.getEntradaB()
+
+        return 0 if a == 1 and b == 1 else 1   
+
+class NorGate(BinaryGate):
+    def __init__(self,n):
+        BinaryGate.__init__(self,n)
+        
+    def performGateLogic(self):
+        a = self.getEntradaA()
+        b = self.getEntradaB()
+
+        return 1 if a == 0 and b == 0 else 0
+
 class Connector:
     def __init__(self, fgate, tgate): #valores fluem da saida de uma porta para a entrada de outra porta
         self.fromgate = fgate
@@ -102,15 +122,28 @@ class Connector:
         return self.togate
 
 
+#def main():
+#   g1 = AndGate("G1")
+#   g2 = AndGate("G2")
+#   g3 = OrGate("G3")
+#   g4 = NotGate("G4")
+#   c1 = Connector(g1,g3)
+#   c2 = Connector(g2,g3)
+#   c3 = Connector(g3,g4)
+#   print(g4.getOutput())
+
+#main()
+
 def main():
    g1 = AndGate("G1")
    g2 = AndGate("G2")
-   g3 = OrGate("G3")
-   g4 = NotGate("G4")
-   c1 = Connector(g1,g3)
-   c2 = Connector(g2,g3)
-   c3 = Connector(g3,g4)
-   print(g4.getOutput())
+   g5 = NorGate('G5')
+   g6 = NandGate('G6')
+   c1 = Connector(g1,g5)
+   c2 = Connector(g2,g5)
+   c3 = Connector(g1,g6)
+   c4 = Connector(g2,g6)
+   print(g6.getOutput())
 
 main()
     
