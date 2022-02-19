2baa5fd8fbcb2526e485f8eff5b4dd42545e31ad
Raf-RM <rafael.r.mateus@gmail.com>
2022-02-19 17:16:19 -0300

    Exercício classe Fração 19/02/22

--- Exercicios-Simples-Python/class-fraction.py
+++ Exercicios-Simples-Python/class-fraction.py
@@ -1,21 +1,28 @@
 # -- Criando uma clase Fraction que constroi um numeral fração do tipo 1/3
 
-# -- Para reduzir as frações vamos contruir um método que calcule o MDC
-def mdc(a, b):
-    while a%b != 0:
-        a_velho = a
-        b_velho = b
-
-        a = b_velho
-        b = a_velho%b_velho
-
-    return b
-
-
 class Fraction:
     def __init__(self, numerador, denominador):
-        self.num = numerador
-        self.den = denominador
+# -- Para reduzir as frações vamos contruir um método que calcule o MDC 
+        if type(numerador) == type(denominador) == int:       
+            a = numerador
+            b = denominador
+            while a%b != 0:
+                a_velho = a
+                b_velho = b
+
+                a = b_velho
+                b = a_velho%b_velho
+                
+            self.num = numerador//b
+            self.den = denominador//b
+        else:
+            raise RuntimeError('O numerador e o denominador da fração devem ser números inteiros!')
+
+    def getNum(self):
+        return self.num
+    
+    def getDen(self):
+        return self.den
 
 # -- Definindo uma função show para podermos imprimir o objeto em Fraction como string    
     def show(self):
@@ -29,8 +36,7 @@ class Fraction:
     def __add__(self, outro):
         novo_num  = self.num * outro.den + self.den * outro.num
         novo_den = self.den * outro.den
-        d_comum = mdc(novo_num, novo_den)
-        return Fraction(novo_num//d_comum, novo_den//d_comum)
+        return Fraction(novo_num, novo_den)
 
 # -- Subscrevendo o método de igualdade (__eq__) para que possamos criar igualdade profunda
     def __eq__(self, outro):
@@ -44,24 +50,39 @@ class Fraction:
     def __mul__(self, outro):
         novo_num = self.num * outro.num
         novo_den = self.den * outro.den
-        d_comum = mdc(novo_num, novo_den)
-        return Fraction(novo_num//d_comum, novo_den//d_comum)
+        return Fraction(novo_num, novo_den)
 
 # -- Implementando a função de divisão
     def __truediv__(self, outro):
         novo_num = self.num * outro.den
         novo_den = self.den * outro.num
-        d_comum = mdc(novo_num, novo_den)
-        return Fraction(novo_num//d_comum, novo_den//d_comum)
+        return Fraction(novo_num, novo_den)
+
 # -- Implementando a função subtração
     def __sub__(self, outro):
         novo_num = self.num * outro.den - self.den * outro.num
         novo_den = self.den * outro.den
-        d_comum = mdc(novo_num, novo_den)
-        return Fraction(novo_num//d_comum, novo_den//d_comum)
+        return Fraction(novo_num, novo_den)
 
 # -- Vamos implementar agora as função de maior e menor (__gt__) e (__lt__) respectivamente
-#    (FALTA IMPLEMENTAR)
+    def __gt__(self, outro):
+        return True if ((self.num/self.den) - (outro.num/outro.den)) > 0 else False
+    
+    def __lt__(self, outro):
+        return True if ((self.num/self.den) - (outro.num/outro.den)) < 0 else False
+
+# -- Vamos implementar agora as função de maior ou igual, menor ou igual e não igual 
+#    (__ge__), (__le__) e (__ne__) respectivamente
+    def __ge__(self, outro):
+        return True if ((self.num/self.den) - (outro.num/outro.den)) >= 0 else False 
+
+    def __le__(self, outro):
+        return True if ((self.num/self.den) - (outro.num/outro.den)) <= 0 else False   
+
+    def __ne__(self,outro):
+        return True if ((self.num/self.den) - (outro.num/outro.den)) == 0 else False    
+
+
 
 
 myFraction = Fraction(3, 5)
@@ -87,3 +108,20 @@ f9 = f1.__sub__(f2)
 f10 = f1 - f2
 print('f9 =',f9)
 print('f10 =',f10)
+f11 =  f2.__gt__(f1)
+f12 = f2 < f1
+print('f11 =', f11)
+print('f12 =', f12)
+f1 = Fraction(1,2)
+f13 = f1.__ge__(f2)
+f14 = f1 <= f2
+f15 = f1 == f2
+f1 = Fraction(3,4)
+f16 = f1 == f2
+print('f13 =', f13)
+print('f14 =', f14)
+print('f15 =', f15)
+print('f16 =', f16)
+print('Numerador = ',f1.getNum(),'e Denominador =',f1.getDen())
+
+f1 = Fraction(0.5,8)
